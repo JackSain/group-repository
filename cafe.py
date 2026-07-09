42178b93c741a7ee3118a14447281c2a363560fe
phillip t <raexc47@gmail.com>
2026-07-09 13:26:37 -0700

    Add files via upload

---
+++ cafe.py
@@ -0,0 +1,6 @@
+drinks = ["coffee", "latte", "espresso", "hot chocolate"]
+food = ["sandwiches", "croissants", "yogurt", "cookies," "brownies"]
+bar = "*"*20
+print(bar, "Menu", bar)
+print(f"Drinks: {drinks} \n" 
+      f"Food: {food}")
